6d2992f639fb3f97021f60829b80ad0b2d021992
touros91 <andreageraci91@gmail.com>
2021-09-13 11:19:08 +0200

    Aggiunto parte HTML, CSS e JS

---
+++ js/main.js
@@ -0,0 +1,125 @@
+// Milestone 1
+// Partendo dalla seguente struttura dati , mostriamo in pagina tutte le icone disponibili come da layout.
+
+const icons = [
+	{
+	  name: 'apple-alt',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'ice-cream',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'fish',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'lemon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'hamburger',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'pizza-slice',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "food"
+	},
+	{
+	  name: 'beer',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-whiskey',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'wine-bottle',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'cocktail',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'coffee',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'glass-martini',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "beverage"
+	},
+	{
+	  name: 'dragon',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'kiwi-bird',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'frog',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'hippo',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'otter',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+	{
+	  name: 'horse',
+	  family: 'fas',
+	  prefix: 'fa-',
+	  category: "animal"
+	},
+  ];
+
+const icone = document.getElementById("contenitore-carte");
+
+const cards = icons.forEach(
+	(element) => {
+		icone.innerHTML += `
+		<div class="icons"> 
+		  	<i class="${element.family} ${element.prefix}${element.name}"></i>
+			<div class="black">${element.category}</div>
+		</div>`;
+	}
+);
